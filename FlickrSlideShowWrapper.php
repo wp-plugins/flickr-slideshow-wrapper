d93b0eea7f239b7cde6d232494fd3a29d3f1841d
jeannot.muller <jeannot.muller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-22 17:07:30 +0000

    adding first version of FSSW
    git-svn-id: https://plugins.svn.wordpress.org/flickr-slideshow-wrapper/trunk@47404 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ FlickrSlideShowWrapper.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Plugin Name: Flickr-SlideShow-Wrapper
+Plugin URI: http://www.ramgad.com
+Description: Including standard flickr slideshow into your blog. Call your FlickrSlideshow by adding &lt;set_id="XXX"&gt; to your content. Please do not forget to replace XXX by the set-id of the flickr-set you want to implement (<a href="http://www.flickr.com">www.flickr.com</a>).
+Version: 1.0
+Author: Dr. Jeannot Muller
+Author URI: http://www.ramgad.com/
+Update Server: http://www.ramgad.com/wp-content/download/wp/
+Min WP Version: 2.5
+Max WP Version: 2.5.1
+*/
+
+// Update routines
+	if ('insert' == $HTTP_POST_VARS['action']) {
+    	update_option("fssw_width",$HTTP_POST_VARS['fssw_width']);
+    	update_option("fssw_height",$HTTP_POST_VARS['fssw_height']);
+    	update_option("fssw_border",$HTTP_POST_VARS['fssw_border']);
+    	update_option("fssw_scroll",$HTTP_POST_VARS['fssw_scroll']);
+	}
+
+if (!class_exists('fssw_main')) {
+    class fssw_main {
+		/**
+		* PHP 4 Compatible Constructor
+		*/
+		function fssw_main(){$this->__construct();}
+		
+		/**
+		* PHP 5 Constructor
+		*/		
+		function __construct(){
+			// Registrieren der WordPress-Hooks
+			add_action('admin_menu', 'fssw_description_add_menu');
+			add_filter('the_content', 'get_flickr_set_id');
+		}
+		// Registration of WordPress-Hooks
+	}
+}
+function fssw_description_option_page() {
+	?>
+
+	<!-- Start Options Adminarea (xhtml) -->
+	  <div class="wrap">
+	    <h2>Flickr-SlideShow-Wrapper Options</h2>
+		         <div style="margin-top:20px;">
+		         <form action="" method="post" action="<?php echo $_SERVER["REQUEST_URI"]; ?>">
+		                          <div style="">
+						<table class="form-table">
+						<tr><th scope="column" colspan="3">Settings</th></tr>
+
+		                <tr><th scope="row">Width:</th><td>
+		                    <input name="fssw_width" size="3" value="<?=get_option("fssw_width");?>" type="text" /></td></tr>
+						<tr><th scope="row">Height:</th><td>
+		                    <input name="fssw_height" size="3" value="<?=get_option("fssw_height");?>" type="text" /></td></tr>
+						<tr><th scope="row">Border:</th><td>
+			                <input name="fssw_border" size="1" value="<?=get_option("fssw_border");?>" type="text" /></td></tr>
+						<tr><th scope="row">Scrolling (yes|no):</th><td>
+				            <input name="fssw_scroll" size="3" value="<?=get_option("fssw_scroll");?>" type="text" /></td></tr>
+				</table>
+	      <br>
+	      <p class="submit"><input name="submit" type="submit" id="submit" value="Save changes &raquo;"></p>
+	     <input class="Submit" name="action" value="insert": type="hidden" />
+	    </form>
+	  </div>
+	</div>
+	<p style="text-align:justify;">Call your FlickrSlideshow by adding &lt;set_id="XXX"&gt; to your content. Please do not forget to replace XXX by the set-id of the flickr-set you want to implement (<a href="http://www.flickr.com">www.flickr.com</a>).</p>
+	<p style="text-align:justify;">If you have problems with WP-SlideShowFlickr, please feel free to contact me: <a href="mailto:jeannot.muller@ramgad.com">jeannot.muller@ramgad.com</a></p>
+
+<?php
+
+} // Ende Funktion fssw_description_option_page()
+
+// Adminmenu Optionen erweitern
+function fssw_description_add_menu() {
+	  global $fssw_width, $fssw_height, $fssw_border, $fssw_scroll; 
+      add_options_page('FSSW', 'FSSW', 9, __FILE__, 'fssw_description_option_page'); //optionenseite hinzufügen
+}
+
+function get_flickr_set_id($content) {
+		 $fssw_width   = get_option('fssw_width');
+		 $fssw_height  = get_option('fssw_height');
+		 $fssw_border  = get_option('fssw_border');
+		 $fssw_scroll  = get_option('fssw_scroll');
+
+
+	// standard initialisation (default values)	
+		 if ( $fssw_width == null or $fssw_height == null) {
+			$fssw_width  = "450";
+		    $fssw_height = "450";		
+		 }
+
+	// parse and replace	 
+	         $content = preg_replace('/<set_id="([a-z0-9_]+)"\/?>/', '<iframe width="' . $fssw_width . '" height="' . $fssw_height . '" src="http://www.flickr.com/slideShow/index.gne?set_id=$1" frameBorder="' . $fssw_border . '" scrolling="' . $fssw_scroll . '"></iframe>', $content);
+        return $content;
+    }
+
+
+
+//instantiate the class
+if (class_exists('fssw_main')) {
+	$fssw_main = new fssw_main();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Flickr-SlideShow-Wrapper ===
+Contributors: jeannot.muller
+Donate link: http://www.ramgad.com/
+Tags: flickr, gallery, slideshow, wrapper, inline, pictures, images
+Requires at least: 2.5.0
+Tested up to: 2.5.1
+Stable tag: 1.0
+
+Including standard flickr slideshow into your blog. Call your FlickrSlideshow by adding <set_id="XXX"> to your content. 
+Please do not forget to replace XXX by the set-id of the flickr-set you want to implement (www.flickr.com).
+
+== Description ==
+
+I did not find any gallery which fit to my expectations. I do not private pictures from flickr to be included to
+my block, as once included they are not private anymore. That's why I wanted to use the standard functionality
+of flickr to embed slideshows.
+
+You can change the border, scrolling (yes/no), and width and height of the linked iframe under settings.
+
+== Installation ==
+1. Upload `FlickrSlideShowWrapper.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Customize the settings under /Settings/FSSW/
+4. Place `<set_id="123213231231231">` in your content
+
+== Frequently Asked Questions ==
+No FAQ available yet.
+
+== Screenshots ==
+
+1. screenshot-1.jpg
+
+== Arbitrary section ==
+Written by Dr. Jeannot Muller, please feel free to contact me under jeannot.muller@ramgad.com
