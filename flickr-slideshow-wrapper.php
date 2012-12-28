3c27ce4bff9501e05c709ed50519e4e3cd0a6359
jeannot.muller <jeannot.muller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-28 12:59:43 +0000

    5.2.2
    git-svn-id: https://plugins.svn.wordpress.org/flickr-slideshow-wrapper/tags/5.2.2@645349 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr-slideshow-wrapper.php
+++ flickr-slideshow-wrapper.php
@@ -3,22 +3,24 @@
 Plugin Name: flickr-slideshow-wrapper
 Plugin URI: http://www.ramgad.com/board7-code-is-poetry/board8-wordpress/3-flickr-slideshow-wrapper/
 Description: Including flickr slideshows. Call fssw by adding [set_id=XYZ] (recommended!) or[set_id=XYZ] (recommended!) or &lt;set&#95;id="XYZ"&gt; to your content. Please do not forget to replace XYZ by the set-id of the flickr-set you want to implement (<a href="htt://www.flickr.com" target="_blank">flickr.com</a>). With &lt;set&#95;tag="tag1, tag2, etc."&gt; your are able to compile your own tag-based set from flickr. Please refer to <a href="http://idgettr.com/" target="_blank">idgettr.com</a> to get the relevant information. You can as well implement a <a href="http://www.slideflickr.com" target="_blank">slideflickr.com</a> show by putting the slideflickr id into &lt;slidef="XYZ"&gt;. For your convenience you can as well use [set_id=XYZ] and/or [slidef=XYZ]. Version 5.0.1 or higher are requesting PHP5!
-Version: 5.2.0
+Version: 5.2.2
 Author: Jeannot Muller
-Author URI: http://www.ramgad.com/board7-code-is-poetry/board8-wordpress/3-flickr-slideshow-wrapper/
+Author URI: http://www.ramgad.com/fssw/
 Min WP Version: 2.5
 Max WP Version: 3.5 
 */
 
 // Update routines
+if ( isset($_POST['action_fssw']) ) {
 	if ('insert' == $_POST['action_fssw']) {
-    	update_option("fssw_width",$_POST['fssw_width']);
-    	update_option("fssw_height",$_POST['fssw_height']);
-    	update_option("fssw_border",$_POST['fssw_border']);
-    	update_option("fssw_scroll",$_POST['fssw_scroll']);
-        update_option("fssw_sfli_w",$_POST['fssw_sfli_w']);
-        update_option("fssw_sfli_h",$_POST['fssw_sfli_h']);
-        update_option("fssw_userid",$_POST['fssw_userid']);
+		update_option("fssw_width",$_POST['fssw_width']);
+		update_option("fssw_height",$_POST['fssw_height']);
+		update_option("fssw_border",$_POST['fssw_border']);
+		update_option("fssw_scroll",$_POST['fssw_scroll']);
+		update_option("fssw_sfli_w",$_POST['fssw_sfli_w']);
+		update_option("fssw_sfli_h",$_POST['fssw_sfli_h']);
+		update_option("fssw_userid",$_POST['fssw_userid']);
+    }
 }
 
 if (!class_exists('fssw_main')) {
@@ -74,7 +76,7 @@ function fssw_description_option_page() {
 	<p style="text-align:justify;">Call your flickr-slideshow-wrapper by adding &lt;set_id="XYZ"&gt; to your content. Please do not forget to replace XYZ by the set-id of the flickr-set you want to implement (<a href="http://www.flickr.com" target="_blank">flickr.com</a>).</p>
 	<p style="text-algin:justify;">With &lt;set&#95;tag="tag1, tag2, etc."&gt; your are able to compile your own tag-based set from flickr. You need to specify your flickr user-id under settings to get this feature look for tags in your library only. Please refer to <a href="http://idgettr.com/" target="_blank">idgettr.com</a> to get the relevant information.</p>
 	<p style="text-algin:justify;">You can call as well a  <a href="http://www.slideflickr.com/" target="_blank">slideflickr.com</a> slideshow by putting the slideflickr id into &lt;slidef="XYZ"&gt;. Please be informed that slideflickr needs the same width and height values you defined during creation of your slideshow on their webpage - otherwise you'll see an endless loading bar.
-	<p style="text-align:justify;">If you have problems with FSSW, please feel free to drop a comment at: <a href="http://www.ramgad.com/software/wordpress/wordpress-plugins/">http://www.ramgad.com/software/wordpress/wordpress-plugins/</a></p>
+	<p style="text-align:justify;">If you have problems with FSSW, please feel free to drop a comment at: <a href="http://www.ramgad.com/fssw/">http://www.ramgad.com/fssw/</a></p>
 
 <?php
 
