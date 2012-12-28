0faad05448ca8f9a982ed7eba47f35d4421cb21c
jeannot.muller <jeannot.muller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-28 12:52:58 +0000

    5.2.1
    git-svn-id: https://plugins.svn.wordpress.org/flickr-slideshow-wrapper/tags/5.2.1@645347 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr-slideshow-wrapper.php
+++ flickr-slideshow-wrapper.php
@@ -3,22 +3,24 @@
 Plugin Name: flickr-slideshow-wrapper
 Plugin URI: http://www.ramgad.com/board7-code-is-poetry/board8-wordpress/3-flickr-slideshow-wrapper/
 Description: Including flickr slideshows. Call fssw by adding [set_id=XYZ] (recommended!) or[set_id=XYZ] (recommended!) or &lt;set&#95;id="XYZ"&gt; to your content. Please do not forget to replace XYZ by the set-id of the flickr-set you want to implement (<a href="htt://www.flickr.com" target="_blank">flickr.com</a>). With &lt;set&#95;tag="tag1, tag2, etc."&gt; your are able to compile your own tag-based set from flickr. Please refer to <a href="http://idgettr.com/" target="_blank">idgettr.com</a> to get the relevant information. You can as well implement a <a href="http://www.slideflickr.com" target="_blank">slideflickr.com</a> show by putting the slideflickr id into &lt;slidef="XYZ"&gt;. For your convenience you can as well use [set_id=XYZ] and/or [slidef=XYZ]. Version 5.0.1 or higher are requesting PHP5!
-Version: 5.2.0
+Version: 5.2.1
 Author: Jeannot Muller
-Author URI: http://www.ramgad.com/board7-code-is-poetry/board8-wordpress/3-flickr-slideshow-wrapper/
+Author URI: http://www.ramgad.com/fssw/
 Min WP Version: 2.5
 Max WP Version: 3.5 
 */
 
 // Update routines
+if ( isset($_POST['action_fssw']) ) {
 	if ('insert' == $_POST['action_fssw']) {
-    	update_option("fssw_width",$_POST['fssw_width']);
-    	update_option("fssw_height",$_POST['fssw_height']);
-    	update_option("fssw_border",$_POST['fssw_border']);
-    	update_option("fssw_scroll",$_POST['fssw_scroll']);
-        update_option("fssw_sfli_w",$_POST['fssw_sfli_w']);
-        update_option("fssw_sfli_h",$_POST['fssw_sfli_h']);
-        update_option("fssw_userid",$_POST['fssw_userid']);
+		update_option("fssw_width",$_POST['fssw_width']);
+		update_option("fssw_height",$_POST['fssw_height']);
+		update_option("fssw_border",$_POST['fssw_border']);
+		update_option("fssw_scroll",$_POST['fssw_scroll']);
+		update_option("fssw_sfli_w",$_POST['fssw_sfli_w']);
+		update_option("fssw_sfli_h",$_POST['fssw_sfli_h']);
+		update_option("fssw_userid",$_POST['fssw_userid']);
+    }
 }
 
 if (!class_exists('fssw_main')) {
